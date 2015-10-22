ca92b1feb0e5396f1daa47e09c08628c33fc15e2
Rory-Powell <rpowell193@gmail.com>
2015-10-22 18:31:09 +0100

    first commit

---
+++ src/main/java/main.java
@@ -0,0 +1,17 @@
+import info.blockchain.api.APIException;
+import network.Requests;
+
+import java.io.IOException;
+
+public class main {
+
+   public static String BLOCK_HASH = "0000000000000bae09a7a393a8acded75aa67e46cb81f7acaa5ad94f9eacd103";
+
+   public static void main(String[] args) {
+      try {
+         String block = Requests.getBlock(BLOCK_HASH);
+      } catch (APIException | IOException e) {
+         e.printStackTrace();
+      }
+   }
+}
