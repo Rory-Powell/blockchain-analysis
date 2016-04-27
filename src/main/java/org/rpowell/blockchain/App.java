315b4d3914ccdc1814d321f7496007ef81950f01
Rory Powell <rpowell03@qub.ac.uk>
2016-04-27 20:27:06 +0100

    Continuous integration test commit 2

--- src/main/java/org/rpowell/blockchain/App.java
+++ src/main/java/org/rpowell/blockchain/App.java
@@ -20,6 +20,6 @@ public class App implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		log.info("Started Application");
-		// CI Test
+		// CI Test 2
 	}
 }
